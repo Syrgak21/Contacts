1eae8bc4b21070cf70ad2650ada75fc8b8e58c34
Syrgak <Syrgak21@gmail.com>
2021-03-03 16:43:57 +0600

    Static

--- contacts/settings.py
+++ contacts/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = 'ir$g*en0u*m==yyz9#ocexk$z56bh8gxiq_!8$jg#!d4v1g1v+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
